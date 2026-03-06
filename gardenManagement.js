a1115c521f1a267094e35bf4c55552c3aa02ff73
amercado1199 <amercado1199@gmail.com>
2026-03-05 22:20:53 -0500

    Completed garden management lab

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,22 @@
-const temperature = 90;
-const timeOfDay = "morning";
-let soilMoisture = 30;
+const temperature = 90
+const timeOfDay = "morning "
+let soilMoisture = 30
+
+if (temperature> 80) {
+    console.log("Watering on");
+} else {
+    console.log("Watering off");
+}
+
+if (timeOfDay === "morning") {
+    console.log("Lights on");
+} else {
+    console.log("Lights off");
+}
+soilMoisture = 0
+
+while (soilMoisture < 40) {
+    soilMoisture += 5;
+    console.log(soilMoisture);
+}
+
